a13a94e004066aef6480640dd3bdf681aee916be
Muiz Safdar <muiz.safder125@gmail.com>
2024-05-08 15:06:13 +0500

    Updated React components

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js',
   ],
 
   theme: {
@@ -13,5 +14,7 @@ module.exports = withMT({
       abc: ["Lato", "sans-serif"]
     }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 });
